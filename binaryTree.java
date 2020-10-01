87ecdbcc5709ab86b00fe49fab9a2f42b66d93ac
ParidaRMG <rp903848@taz.cs.wcupa.edu>
2020-09-30 23:38:23 -0400

    Added TreeInsert function

--- binaryTree.java
+++ binaryTree.java
@@ -1,13 +1,17 @@
 class Node
 {
-     String element;
-     Node left;            // Left child
-     Node right;           // Right child
-     Node(String e, Node left, Node right)
-     {
-         element = e;
-         left = null;
-         right = null;
-     }
-}
+  String element;
+  Node left;            // Left child
+  Node right;           // Right child
+  Node(String e, Node left, Node right) {
+      element = e;
+      left = null;
+      right = null;
+  }
 
+  public static Node TreeInsert(Node root, Node newNode) {
+    return root;
+  }
+
+
+}
