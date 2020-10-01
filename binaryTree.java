c2dd3d612f287220778e8f3b56586f7b491d978f
ParidaRMG <rp903848@taz.cs.wcupa.edu>
2020-09-30 23:35:07 -0400

    Added binaryTree.java

---
+++ binaryTree.java
@@ -0,0 +1,13 @@
+class Node
+{
+     String element;
+     Node left;            // Left child
+     Node right;           // Right child
+     Node(String e, Node left, Node right)
+     {
+         element = e;
+         left = null;
+         right = null;
+     }
+}
+
